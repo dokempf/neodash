ebe153221c0297a652fe9a33d73f0b4f2a869fd6
jmsssc <sk353@uni-heidelberg.de>
2025-08-26 11:10:33 +0200

    Fix default icon

--- src/chart/map/layers/MarkerLayer.tsx
+++ src/chart/map/layers/MarkerLayer.tsx
@@ -139,7 +139,7 @@ export function createMarkers(data, props) {
           <div
             style={{ color: node.color, textAlign: 'center', marginTop: markerMarginTop, marginLeft: markerMarginLeft }}
           >
-            <i className={`fa fa-${node.properties.icon || 'heart'} ${markerIconClass}`} />
+            <i className={`fa fa-${node.properties.icon || 'location-dot'} ${markerIconClass}`} />
           </div>
         }
       >
